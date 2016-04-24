dba10970e09209cb57bc678b7444942aa45a1699
nurre69 <niika.nurmimaa@gmail.com>
2016-04-24 22:38:58 +0300

    Added files via upload

--- confirm.php
+++ confirm.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <?php
 session_start();
+	$title = "Varmista syöttämäsi tiedot";
+	include_once 'header2.php';
 if($_SESSION["logged_in"] == 'yes'){
 	include('connection.php');
-
         if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
 		session_unset();
 		session_destroy();
@@ -13,7 +14,6 @@ if($_SESSION["logged_in"] == 'yes'){
 	header("location: error.php");
 }
 $_SESSION['LAST_ACTIVITY'] = time();
-
 function test_input($data) 
 {
 	$data = trim($data);
@@ -23,114 +23,112 @@ function test_input($data)
 }
 include 'connection.php';
 $ssn = $_SESSION["ssn"];
-$sql = "select m.typeid, mt.measurement_unit, m.value AS Arvo, m.timestamp AS Aika, mt.measurement_name
-from measurements m
-    join (select max(timestamp) maxtimestamp, typeid
-          from measurements 
-          group by typeid) m2 on m.timestamp = m2.maxtimestamp
-                             and m.typeid = m2.typeid
-    join measurement_type mt on m.typeid = mt.typeid
-where m.ssn = '$ssn'
-";
+$sql = "SELECT max(m.timestamp) AS Aika, m.typeid, m.value AS Arvo, m_t.measurement_name, m_t.measurement_unit
+					FROM measurements m
+					INNER JOIN measurement_type m_t on m.typeid = m_t.typeid
+					WHERE m.typeid = 1 AND m.ssn = '$ssn'";
 $result = $conn->query($sql);
 if($result->num_rows > 0) 
 		{
-			
 			while($row = $result->fetch_assoc()) 
 			{	
-				if($row["measurement_name"] == 'glucose') 
-				{	
-					$mittaus = "Verensokeri: "; 
+					$mittaus1 = "Verensokeri: "; 
 					$row["Arvo"] = str_replace(',', '.', $row["Arvo"]);
-					$tulos1 = $mittaus . $row["Arvo"]." ".$row["measurement_unit"].", mittausaika: ".$row["Aika"]."<br>"."<br>";
-				}
-				elseif($row["measurement_name"] == 'pressure') 
-				{	
-					$mittaus = "Verenpaine: ";
+					$tulos1 = $row["Arvo"]." ".$row["measurement_unit"].", mittausaika: ".$row["Aika"]."<br>";
+			}
+		}
+$sql = "SELECT max(m.timestamp) AS Aika, m.typeid, m.value AS Arvo, m_t.measurement_name, m_t.measurement_unit
+					FROM measurements m
+					INNER JOIN measurement_type m_t on m.typeid = m_t.typeid
+					WHERE m.typeid = 2 AND m.ssn = '$ssn'";
+$result = $conn->query($sql);
+if($result->num_rows > 0) 
+		{
+			while($row = $result->fetch_assoc()) 
+			{	
+					$mittaus2 = "Verenpaine: ";
 					$paine = explode("/",$row["Arvo"]);					
 					$row["Arvo"] = str_replace(',', '.', $row["Arvo"]);
-					$tulos2 = $mittaus . $row["Arvo"]." ".$row["measurement_unit"].", mittausaika: ".$row["Aika"]."<br>"."<br>";
-				}
-				elseif($row["measurement_name"] == 'temperature' && $row["measurement_unit"] == 'C') 
-				{	
-					$mittaus = "Lämpötila :"; 
+					$tulos2 = $row["Arvo"]." ".$row["measurement_unit"].", mittausaika: ".$row["Aika"]."<br>";
+			}
+		}
+$sql = "SELECT max(m.timestamp) AS Aika, m.typeid, m.value AS Arvo, m_t.measurement_name, m_t.measurement_unit
+					FROM measurements m
+					INNER JOIN measurement_type m_t on m.typeid = m_t.typeid
+					WHERE m.typeid = 3 AND m.ssn = '$ssn'";
+$result = $conn->query($sql);
+if($result->num_rows > 0) 
+		{
+			while($row = $result->fetch_assoc()) 
+			{	
+					$mittaus3 = "Lämpötila :"; 
 					$row["Arvo"] = str_replace(',', '.', $row["Arvo"]);
-					$tulos3 = $mittaus . $row["Arvo"]." &deg".$row["measurement_unit"].", mittausaika: ".$row["Aika"]."<br>"."<br>";
-				}
-				elseif($row["measurement_name"] == 'weight') 
-				{
-					$mittaus = "Paino: ";
+					$tulos3 = $row["Arvo"]." &deg".$row["measurement_unit"].", mittausaika: ".$row["Aika"]."<br>";
+			}
+		}
+$sql = "SELECT max(m.timestamp) AS Aika, m.typeid, m.value AS Arvo, m_t.measurement_name, m_t.measurement_unit
+					FROM measurements m
+					INNER JOIN measurement_type m_t on m.typeid = m_t.typeid
+					WHERE m.typeid = 5 AND m.ssn = '$ssn'";
+$result = $conn->query($sql);
+if($result->num_rows > 0) 
+		{
+			while($row = $result->fetch_assoc()) 
+			{	
+					$mittaus4 = "Paino: ";
 					$row["Arvo"] = str_replace(',', '.', $row["Arvo"]);
 					$paino = $row["Arvo"]; 
-					$tulos4 = $mittaus . $row["Arvo"]." ".$row["measurement_unit"].", mittausaika: ".$row["Aika"]."<br>"."<br>";
-				}
-				elseif($row["measurement_name"] == 'height') 
-				{	
-					$mittaus = "Pituus: "; 
+					$tulos4 = $row["Arvo"]." ".$row["measurement_unit"].", mittausaika: ".$row["Aika"]."<br>";
+			}
+		}
+$sql = "SELECT max(m.timestamp) AS Aika, m.typeid, m.value AS Arvo, m_t.measurement_name, m_t.measurement_unit
+					FROM measurements m
+					INNER JOIN measurement_type m_t on m.typeid = m_t.typeid
+					WHERE m.typeid = 6 AND m.ssn = '$ssn'";
+$result = $conn->query($sql);
+if($result->num_rows > 0) 
+		{
+			while($row = $result->fetch_assoc()) 
+			{	
+					$mittaus5 = "Pituus: "; 
 					$row["Arvo"] = str_replace(',', '.', $row["Arvo"]);
 					$pituus = $row["Arvo"];				
-					$tulos5 = $mittaus . $row["Arvo"]." ".$row["measurement_unit"].", mittausaika: ".$row["Aika"];
-
-				}		
-			} 
-			
-		}
+					$tulos5 = $row["Arvo"]." ".$row["measurement_unit"].", mittausaika: ".$row["Aika"]."<br>";	
+			}		
+		} 
 ?>
-<html lang="en">
-	<head>
-		<title>onlinedoc -lääkäripalvelu</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link rel="stylesheet" type="css" media="screen" href="tyyli.css" />
-		<link href='https://fonts.googleapis.com/css?family=Arimo' rel='stylesheet' type='text/css'>
-	</head>
-<body>
-<div class="bg">
-<div style="margin-left: 20%; margin-right: 20%;">
-<img class="c" src="drlogo.png">
-</div>
-<div class="menu"
-<ul class="menu">
-	<li><a href="home.php">> Etusivu</a></li>
-	<li><a href="#"><span class="active">> Mittaustulokset</span></a>
-		<ul>
-		<li><a href="insert.php">> Syötä mittaustuloksia</a></li>
-		<li><a href="search.php">> Hae mittaustuloksia</a></li>
-		</ul>
-	</li>
-	<li><a href="service.php">> Tulosten tulkinta</a></li>
-	<li><a href="profile.php">> Henkilötiedot</a></li>
-	<li style="float: right;"><a href="logout.php">> Kirjaudu ulos</a></li>
-</ul>
-</div>
 <div class="data">
-<a class="tooltip" href="#"><img src="question.png"><span>Tarkista syöttämäsi tiedot, kun olet valmis paina 'Seuraava'!</span></a>
+<a class="tooltip" href="#"><img src="question.png" alt="Ohje!"><span>Tarkista syöttämäsi tiedot, kun olet valmis paina 'Seuraava'!</span></a>
 <div class="cc">
 <h2>Vaihe 2; tarkista syöttämäsi tiedot: </h2>
 <div class="laatikko1">
-<?php 
-echo $tulos1;
-echo $tulos2;
-echo $tulos3;
-echo $tulos4;
-echo $tulos5;
- ?>
-</div>	
+<p class="arvo">
+<?php echo $mittaus1; ?>
+</p>
+<?php echo $tulos1; ?>
+<p class="arvo">
+<?php echo $mittaus2; ?>
+</p>
+<?php echo $tulos2; ?>
+<p class="arvo">
+<?php echo $mittaus3; ?>
+</p>
+<?php echo $tulos3; ?>
+<p class="arvo">
+<?php echo $mittaus4; ?>
+</p>
+<?php echo $tulos4; ?>
+<p class="arvo">
+<?php echo $mittaus5; ?>
+</p>
+<?php echo $tulos5; ?><br>
+</div>
 <div class="bc">
 <button type="button" class="button-minimal" onclick="location.href='service.php'">Seuraava</button>
 <button type="button" class="button-minimal" onclick="history.go(-1);return true;">Takaisin</button><br>		
 </div>
 </div>
 </div>
-<div class="menu"
-<ul class="menu">
-    <li><a href="about.php">> Tietoa meistä</a></li>
-	<li><a href="contact.php">> Ota yhteyttä</a></li>
-	<li style="float: right;"><p>Kirjautuneena: <?php echo $_SESSION["fn"] . " " . $_SESSION["ln"]; ?></p></li>
-</ul>
-</div>
-<footer>
-Page created by Metropolia Hyte Ryhmä 6: Nurmimaa, Kuutti, Pakkala. © 2016 
-</footer>
-</div>
-</body>
-</html>
+<?php
+include_once 'footer2.php';
+?>
